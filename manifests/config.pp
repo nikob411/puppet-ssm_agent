0150d610c5fe792ea760b36c15c585665160d6c0
shazi7804 <shazi7804@gmail.com>
2017-06-01 03:22:50 +0800

    fix proxy to http_proxy

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,7 +6,7 @@ class ssm_agent::config inherits ssm_agent {
     notify  => Class['ssm_agent::service'],
   }
 
-  if $ssm_agent::proxy or $ssm_agent::no_proxy {
+  if $ssm_agent::http_proxy or $ssm_agent::no_proxy {
     file { $ssm_agent::systemd_file:
       ensure  => file,
       content => template("${module_name}/amazon-ssm-agent.conf.erb"),
